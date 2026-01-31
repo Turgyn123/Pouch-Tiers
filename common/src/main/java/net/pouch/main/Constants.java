544947dcddef4b4b634493a438c8362d0d416bb2
Turgyn <130315908+Turgyn123@users.noreply.github.com>
2026-01-31 16:43:55 +0100

    set everything up

---
+++ common/src/main/java/net/pouch/main/Constants.java
@@ -0,0 +1,11 @@
+package net.pouch.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "pouch";
+	public static final String MOD_NAME = "Pouch Tiers+";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
